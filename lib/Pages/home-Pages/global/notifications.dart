a1dea9a5edf617c597b2294d2670eccdb7f4b7f6
Nader2004 <71600566+Nader2004@users.noreply.github.com>
2020-12-05 21:56:07 +0200

    updated

--- lib/Pages/home-Pages/global/notifications.dart
+++ lib/Pages/home-Pages/global/notifications.dart
@@ -5,7 +5,12 @@ import 'package:looper/models/notification.dart' as not;
 import 'package:looper/models/userModel.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:looper/Pages/home-Pages/global/profile.dart';
-import 'package:visibility_detector/visibility_detector.dart';
+import 'package:looper/widgets/post.dart';
+import 'package:looper/widgets/talent.dart';
+import 'package:looper/widgets/sport.dart';
+import 'package:looper/widgets/challenge.dart';
+import 'package:looper/widgets/comedy-joke.dart';
+import 'package:looper/Pages/home-Pages/chat/chat_list.dart';
 
 class NotificationsPage extends StatefulWidget {
   NotificationsPage({Key key}) : super(key: key);
@@ -95,22 +100,19 @@ class _NotificationsPageState extends State<NotificationsPage> {
                               return SizedBox.shrink();
                             }
                             final User _user = User.fromDoc(usersnapshot.data);
-                            return VisibilityDetector(
-                              key: Key(_notif.id),
-                              onVisibilityChanged: (VisibilityInfo info) {
-                                final double _percent =
-                                    info.visibleFraction * 100;
-                                if (_percent == 100) {
-                                  FirebaseFirestore.instance
-                                      .collection('users')
-                                      .doc(_userId)
-                                      .collection('notifications')
-                                      .doc(_notif.id)
-                                      .delete();
-                                }
-                              },
-                              child: ListTile(
-                                leading: ClipRRect(
+                            return ListTile(
+                              leading: GestureDetector(
+                                onTap: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => ProfilePage(
+                                        userId: _notif.userId,
+                                      ),
+                                    ),
+                                  );
+                                },
+                                child: ClipRRect(
                                   borderRadius: BorderRadius.circular(50),
                                   child: CachedNetworkImage(
                                     imageUrl: _user.profileImageUrl,
@@ -119,27 +121,209 @@ class _NotificationsPageState extends State<NotificationsPage> {
                                     width: 50,
                                   ),
                                 ),
-                                title: Text(_notif.title),
-                                subtitle: Text(_notif.body),
-                                trailing: Icon(
-                                  Icons.arrow_forward_ios,
-                                  color: Colors.grey,
-                                ),
-                                onTap: () {
-                                  Navigator.of(context).push(
-                                    MaterialPageRoute(
-                                      builder: (context) => ProfilePage(
-                                        userId: _user.id,
-                                      ),
-                                    ),
-                                  );
-                                },
                               ),
+                              title: Text(_notif.title),
+                              subtitle: Text(_notif.body),
+                              trailing: Icon(
+                                Icons.arrow_forward_ios,
+                                color: Colors.grey,
+                              ),
+                              onTap: _notif.navigator == 'special'
+                                  ? null
+                                  : () {
+                                      if (_notif.navigator == 'profile') {
+                                        Navigator.of(context).push(
+                                          MaterialPageRoute(
+                                            builder: (context) => ProfilePage(
+                                              userId: _user.id,
+                                            ),
+                                          ),
+                                        );
+                                      } else if (_notif.navigator == 'chat') {
+                                        Navigator.of(context).push(
+                                          MaterialPageRoute(
+                                            builder: (context) => ChatList(),
+                                          ),
+                                        );
+                                      } else {
+                                        Navigator.of(context).push(
+                                          MaterialPageRoute(
+                                            builder: (context) =>
+                                                NotificationViewPage(
+                                              navigator: _notif.navigator,
+                                              notifBody: _notif.body,
+                                              contentId: _notif.contentId,
+                                              symbol: _notif.symbol,
+                                            ),
+                                          ),
+                                        );
+                                      }
+                                      FirebaseFirestore.instance
+                                          .collection('users')
+                                          .doc(_userId)
+                                          .collection('notifications')
+                                          .doc(_notif.id)
+                                          .delete();
+                                    },
                             );
                           });
                     });
               },
             ),
+    );
+  }
+}
+
+class NotificationViewPage extends StatefulWidget {
+  final String notifBody;
+  final String navigator;
+  final String contentId;
+  final String symbol;
+  NotificationViewPage({
+    Key key,
+    this.notifBody,
+    this.navigator,
+    this.contentId,
+    this.symbol,
+  }) : super(key: key);
+
+  @override
+  _NotificationViewPageState createState() => _NotificationViewPageState();
+}
+
+class _NotificationViewPageState extends State<NotificationViewPage> {
+  DocumentSnapshot _documentSnapshot;
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.navigator.contains('creation')) {
+      if (widget.navigator.contains('post')) {
+        FirebaseFirestore.instance
+            .collection('posts')
+            .where('timestamp', isEqualTo: widget.symbol)
+            .get()
+            .then(
+              (value) => setState(() => _documentSnapshot = value.docs[0]),
+            );
+      } else if (widget.navigator.contains('talent')) {
+        FirebaseFirestore.instance
+            .collection('talents')
+            .where('timestamp', isEqualTo: widget.symbol)
+            .get()
+            .then(
+              (value) => setState(() => _documentSnapshot = value.docs[0]),
+            );
+      } else if (widget.navigator.contains('sport')) {
+        FirebaseFirestore.instance
+            .collection('sports')
+            .where('timestamp', isEqualTo: widget.symbol)
+            .get()
+            .then(
+              (value) => setState(() => _documentSnapshot = value.docs[0]),
+            );
+      } else if (widget.navigator.contains('comedy')) {
+        FirebaseFirestore.instance
+            .collection('comedy')
+            .where('timestamp', isEqualTo: widget.symbol)
+            .get()
+            .then(
+              (value) => setState(() => _documentSnapshot = value.docs[0]),
+            );
+      } else {
+        FirebaseFirestore.instance
+            .collection('challenge')
+            .where('timestamp', isEqualTo: widget.symbol)
+            .get()
+            .then(
+              (value) => setState(() => _documentSnapshot = value.docs[0]),
+            );
+      }
+    } else {
+      if (widget.navigator == 'post') {
+        print('isPost');
+        FirebaseFirestore.instance
+            .collection('posts')
+            .doc(widget.contentId)
+            .get()
+            .then(
+              (value) => setState(() => _documentSnapshot = value),
+            );
+      } else if (widget.navigator == 'talent') {
+        FirebaseFirestore.instance
+            .collection('talents')
+            .doc(widget.contentId)
+            .get()
+            .then(
+              (value) => setState(() => _documentSnapshot = value),
+            );
+      } else if (widget.navigator == 'sport') {
+        FirebaseFirestore.instance
+            .collection('sports')
+            .doc(widget.contentId)
+            .get()
+            .then(
+              (value) => setState(() => _documentSnapshot = value),
+            );
+      } else if (widget.navigator == 'comedy') {
+        FirebaseFirestore.instance
+            .collection('comedy')
+            .doc(widget.contentId)
+            .get()
+            .then(
+              (value) => setState(() => _documentSnapshot = value),
+            );
+      } else {
+        FirebaseFirestore.instance
+            .collection('challenge')
+            .doc(widget.contentId)
+            .get()
+            .then(
+              (value) => setState(() => _documentSnapshot = value),
+            );
+      }
+    }
+  }
+
+  Widget _buildContent() {
+    if (widget.navigator.contains('post')) {
+      return PostWidget(snapshot: _documentSnapshot);
+    } else if (widget.navigator.contains('talent')) {
+      return Talent(data: _documentSnapshot);
+    } else if (widget.navigator.contains('sport')) {
+      return Sport(data: _documentSnapshot);
+    } else if (widget.navigator.contains('challenge')) {
+      return Challenge(data: _documentSnapshot);
+    } else if (widget.navigator.contains('comedy')) {
+      return ComedyJoke(data: _documentSnapshot);
+    } else {
+      return SizedBox.shrink();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0.5,
+        backgroundColor: Colors.white,
+        iconTheme: IconThemeData.fallback(),
+        centerTitle: true,
+        title: Text(
+          widget.notifBody,
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
+      body: _documentSnapshot == null
+          ? Center(
+              child: CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation(
+                  Colors.black,
+                ),
+                strokeWidth: 1.2,
+              ),
+            )
+          : _buildContent(),
     );
   }
 }
