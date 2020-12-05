27da6ef572fe2e136fb130c0983d9f9a7bd2e473
Nader2004 <71600566+Nader2004@users.noreply.github.com>
2020-12-05 21:52:09 +0200

    updated

--- lib/models/notification.dart
+++ lib/models/notification.dart
@@ -4,11 +4,17 @@ class Notification {
   final String id;
   final String title;
   final String body;
+  final String navigator;
+  final String contentId;
+  final String symbol;
   final String userId;
   const Notification({
     this.id,
     this.title,
     this.body,
+    this.navigator,
+    this.contentId,
+    this.symbol,
     this.userId,
   });
 
@@ -17,6 +23,9 @@ class Notification {
       title: notif['notification']['title'],
       body: notif['notification']['body'],
       userId: notif['data']['userId'],
+      navigator: notif['data']['navigator'],
+      contentId: notif['data']['contentId'],
+      symbol: notif['data']['symbol'],
     );
   }
 
@@ -26,6 +35,9 @@ class Notification {
       title: doc.data()['title'],
       body: doc.data()['body'],
       userId: doc.data()['userId'],
+      navigator: doc.data()['navigator'],
+      contentId: doc.data()['contentId'],
+      symbol: doc.data()['symbol'],
     );
   }
 }
