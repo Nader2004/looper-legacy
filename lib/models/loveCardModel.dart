8c5337c01ebebcb7cac3fd2dad2a560ae3416579
Nader2004 <71600566+Nader2004@users.noreply.github.com>
2020-12-05 21:51:32 +0200

    updated

--- lib/models/loveCardModel.dart
+++ lib/models/loveCardModel.dart
@@ -3,24 +3,24 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class LoveCard {
   final String authorId;
   final String authorName;
+  final String authorGender;
   final Map<String, dynamic> authorPersonality;
   final String authorImage;
   final int authorAge;
   final String imageUrl;
   final List<dynamic> qualities;
-  final int compatibility;
   final bool interested;
   final String location;
   final String timestamp;
   const LoveCard({
     this.authorId,
     this.authorName,
+    this.authorGender,
     this.authorPersonality,
     this.authorImage,
     this.authorAge,
     this.imageUrl,
     this.qualities,
-    this.compatibility,
     this.interested,
     this.location,
     this.timestamp,
@@ -30,12 +30,12 @@ class LoveCard {
     return LoveCard(
       authorId: doc.data()['authorId'],
       authorName: doc.data()['authorName'],
+      authorGender: doc.data()['authorGender'],
       authorPersonality: doc.data()['author-personality'],
       authorImage: doc.data()['authorImage'],
       authorAge: doc.data()['authorAge'],
       imageUrl: doc.data()['imageUrl'],
       qualities: doc.data()['qualities'],
-      compatibility: doc.data()['compatibility'],
       interested: doc.data()['interested'],
       location: doc.data()['location'],
       timestamp: doc.data()['timestamp'],
