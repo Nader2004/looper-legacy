f672486a76bc18d36fbe16ce32a22b09906404ff
Nader2004 <71600566+Nader2004@users.noreply.github.com>
2020-12-22 09:28:11 +0200

    updated

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -11,9 +11,4 @@ import Firebase
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
-   
-   override func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-   Messaging.messaging().apnsToken = deviceToken
-   super.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
- }
 }
