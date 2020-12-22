fe1cbedb7db7f5c3607d2f89db37bb56b9e5d7f2
Nader2004 <71600566+Nader2004@users.noreply.github.com>
2020-12-22 08:51:24 +0200

    updated for notifications

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,16 +1,11 @@
 import UIKit
 import Flutter
+import Firebase
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    if #available(iOS 10.0, *) {
-      UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
-    }
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+   override func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+   Messaging.messaging().apnsToken = deviceToken
+   super.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
+ }
 }
