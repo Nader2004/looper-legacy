558c75d44e060f5be14dec6ca6492ea078114fb8
Nader2004 <71600566+Nader2004@users.noreply.github.com>
2021-01-18 12:37:30 +0200

    update

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -14,7 +14,6 @@ import FirebaseMessaging
         }
         
         GeneratedPluginRegistrant.register(with: self)
-        application.registerForRemoteNotifications()
 
         return super.application(application, didFinishLaunchingWithOptions: launchOptions)
     }
