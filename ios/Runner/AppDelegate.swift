9e0603d0eb5b58d14ebf0164d3b4c951d306dfeb
Nader2004 <71600566+Nader2004@users.noreply.github.com>
2021-02-05 18:29:14 +0200

    updated

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -14,7 +14,6 @@ import FirebaseMessaging
         }
         
         GeneratedPluginRegistrant.register(with: self)
-
         return super.application(application, didFinishLaunchingWithOptions: launchOptions)
     }
     
