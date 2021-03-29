ba52d8215d12f8cfbcffb5824c58f30b7b65f97a
Nader2004 <71600566+Nader2004@users.noreply.github.com>
2021-03-29 23:28:03 +0200

    Update messageModel.dart

--- lib/models/messageModel.dart
+++ lib/models/messageModel.dart
@@ -4,15 +4,19 @@ class Message {
   final String id;
   final String author;
   final String peerId;
+  final String seenBy;
   final String content;
   final int type;
+  final bool seen;
   final String timestamp;
   const Message({
     this.id,
     this.author,
     this.peerId,
+    this.seenBy,
     this.content,
     this.type,
+    this.seen,
     this.timestamp,
   });
   factory Message.fromDoc(DocumentSnapshot doc) {
@@ -20,8 +24,10 @@ class Message {
       id: doc.data()['id'],
       author: doc.data()['from'],
       peerId: doc.data()['to'],
+      seenBy: doc.data()['seenBy'],
       content: doc.data()['content'],
       type: doc.data()['type'],
+      seen: doc.data()['seen'],
       timestamp: doc.data()['timestamp'],
     );
   }
