abfdd84abff2975ef4b00423076fbfaa3d458dd6
Nader2004 <71600566+Nader2004@users.noreply.github.com>
2021-04-06 12:16:40 +0200

    Update post-native-ad.dart

--- lib/widgets/post-native-ad.dart
+++ lib/widgets/post-native-ad.dart
@@ -1,9 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_native_admob/flutter_native_admob.dart';
-import 'package:flutter_native_admob/native_admob_options.dart';
-import 'package:flutter_native_admob/native_admob_controller.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 class PostNativeAd extends StatefulWidget {
   PostNativeAd({Key key}) : super(key: key);
@@ -16,38 +14,38 @@ class _PostNativeAdState extends State<PostNativeAd> {
   static const String AndroidAdUnitId =
       'ca-app-pub-9448985372006294/2961269368';
   static const String IOSAdUnitId = 'ca-app-pub-9448985372006294/6816753892';
-  final _nativeAdController = NativeAdmobController();
-  double _height;
+  NativeAd nativeAd;
 
   @override
   void initState() {
-    _nativeAdController.stateChanged.listen(_onStateChanged);
+    nativeAd = NativeAd(
+      adUnitId: Platform.isIOS ? IOSAdUnitId : AndroidAdUnitId,
+      factoryId: 'native-ad',
+      listener: AdListener(
+        onAdFailedToLoad: (ad, error) {
+          nativeAd.dispose();
+        },
+        onApplicationExit: (ad) {
+          nativeAd.dispose();
+        },
+      ),
+      request: AdRequest(),
+    );
+    nativeAd.load();
     super.initState();
   }
 
-  void _onStateChanged(AdLoadState state) {
-    switch (state) {
-      case AdLoadState.loading:
-        setState(() {
-          _height = 0;
-        });
-        break;
-
-      case AdLoadState.loadCompleted:
-        setState(() {
-          _height = MediaQuery.of(context).size.height / 3;
-        });
-        break;
-
-      default:
-        break;
-    }
+  @override
+  void dispose() {
+    nativeAd.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+    final AdWidget adWidget = AdWidget(ad: nativeAd);
     return Container(
-      height: _height,
+      height: MediaQuery.of(context).size.height / 3,
       margin: EdgeInsets.symmetric(horizontal: 10),
       child: Card(
         elevation: 10,
@@ -56,16 +54,7 @@ class _PostNativeAdState extends State<PostNativeAd> {
         ),
         child: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: NativeAdmob(
-            adUnitID: Platform.isAndroid ? AndroidAdUnitId : IOSAdUnitId,
-            controller: _nativeAdController,
-            loading: Container(),
-            options: NativeAdmobOptions(
-              adLabelTextStyle: NativeTextStyle(
-                backgroundColor: Colors.black,
-              ),
-            ),
-          ),
+          child: adWidget,
         ),
       ),
     );
