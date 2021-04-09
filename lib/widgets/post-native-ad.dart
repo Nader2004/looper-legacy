f82ca38ba88f7a81dc459fef84224a880d80646d
Nader2004 <71600566+Nader2004@users.noreply.github.com>
2021-04-09 12:44:57 +0200

    Update post-native-ad.dart

--- lib/widgets/post-native-ad.dart
+++ lib/widgets/post-native-ad.dart
@@ -1,20 +1,4 @@
-import 'dart:io';
-
-import 'package:flutter/material.dart';
-import 'package:google_mobile_ads/google_mobile_ads.dart';
-
-class PostNativeAd extends StatefulWidget {
-  PostNativeAd({Key key}) : super(key: key);
-
-  @override
-  _PostNativeAdState createState() => _PostNativeAdState();
-}
-
-class _PostNativeAdState extends State<PostNativeAd> {
-  static const String AndroidAdUnitId =
-      'ca-app-pub-9448985372006294/2961269368';
-  static const String IOSAdUnitId = 'ca-app-pub-9448985372006294/6816753892';
-  NativeAd nativeAd;
+ bool _isLoaded = false;
 
   @override
   void initState() {
@@ -22,6 +6,11 @@ class _PostNativeAdState extends State<PostNativeAd> {
       adUnitId: Platform.isIOS ? IOSAdUnitId : AndroidAdUnitId,
       factoryId: 'native-ad',
       listener: AdListener(
+        onAdLoaded: (ad) {
+          setState(() {
+            _isLoaded = true;
+          });
+        },
         onAdFailedToLoad: (ad, error) {
           nativeAd.dispose();
         },
@@ -44,19 +33,20 @@ class _PostNativeAdState extends State<PostNativeAd> {
   @override
   Widget build(BuildContext context) {
     final AdWidget adWidget = AdWidget(ad: nativeAd);
-    return Container(
-      height: MediaQuery.of(context).size.height / 3,
-      margin: EdgeInsets.symmetric(horizontal: 10),
-      child: Card(
-        elevation: 10,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(16),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: adWidget,
-        ),
-      ),
-    );
+    return _isLoaded == false
+        ? SizedBox.shrink()
+        : Container(
+            height: MediaQuery.of(context).size.height / 3,
+            margin: EdgeInsets.symmetric(horizontal: 10),
+            child: Card(
+              elevation: 10,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: adWidget,
+              ),
+            ),
+          );
   }
-}
