24d50aeccffc94bca3b0ee70dac10f77bf0c76c2
Nader2004 <71600566+Nader2004@users.noreply.github.com>
2021-04-11 22:46:01 +0200

    Update post-native-ad.dart

--- lib/widgets/post-native-ad.dart
+++ lib/widgets/post-native-ad.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
@@ -16,73 +15,68 @@ class _PostNativeAdState extends State<PostNativeAd> {
       'ca-app-pub-3940256099942544/2247696110'; // 'ca-app-pub-9448985372006294/2961269368';
   static const String IOSAdUnitId =
       'ca-app-pub-3940256099942544/3986624511'; //'ca-app-pub-9448985372006294/6816753892';
-  NativeAd nativeAd;
+  NativeAd _nativeAd;
   final Completer<NativeAd> nativeAdCompleter = Completer<NativeAd>();
 
   @override
-  void initState() {  
-    nativeAd = NativeAd(
-      adUnitId: Platform.isIOS ? IOSAdUnitId : AndroidAdUnitId,
+  void initState() {
+    super.initState();
+    _nativeAd = NativeAd(
+      adUnitId: NativeAd.testAdUnitId,
+      request: AdRequest(),
       factoryId: 'native-ad',
       listener: AdListener(
-        onAdLoaded: (ad) {
+        onAdLoaded: (Ad ad) {
+          print('$NativeAd loaded.');
           nativeAdCompleter.complete(ad as NativeAd);
         },
-        onAdFailedToLoad: (ad, error) {
-          nativeAd.dispose();
+        onAdFailedToLoad: (Ad ad, LoadAdError error) {
+          ad.dispose();
+          print('$NativeAd failedToLoad: $error');
           nativeAdCompleter.completeError(error);
         },
-        onApplicationExit: (ad) {
-          nativeAd.dispose();
-        },
+        onAdOpened: (Ad ad) => print('$NativeAd onAdOpened.'),
+        onAdClosed: (Ad ad) => print('$NativeAd onAdClosed.'),
+        onApplicationExit: (Ad ad) => print('$NativeAd onApplicationExit.'),
       ),
-      request: AdRequest(),
     );
-    Future<void>.delayed(Duration(seconds: 1), () => nativeAd.load());
-    super.initState();
+    Future<void>.delayed(Duration(seconds: 1), () => _nativeAd.load());
   }
 
   @override
   void dispose() {
-    nativeAd.dispose();
     super.dispose();
+    _nativeAd.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<NativeAd>(
-        future: nativeAdCompleter.future,
-        builder: (context, snapshot) {
-          Widget child;
+      future: nativeAdCompleter.future,
+      builder: (BuildContext context, AsyncSnapshot<NativeAd> snapshot) {
+        Widget child;
 
-          switch (snapshot.connectionState) {
-            case ConnectionState.none:
-            case ConnectionState.waiting:
-            case ConnectionState.active:
-              child = Container();
-              break;
-            case ConnectionState.done:
-              if (snapshot.hasData) {
-                child = AdWidget(ad: nativeAd);
-              } else {
-                child = Text('Error loading $NativeAd');
-              }
-          }
+        switch (snapshot.connectionState) {
+          case ConnectionState.none:
+          case ConnectionState.waiting:
+          case ConnectionState.active:
+            child = Container();
+            break;
+          case ConnectionState.done:
+            if (snapshot.hasData) {
+              child = AdWidget(ad: _nativeAd);
+            } else {
+              child = Text('Error loading $NativeAd');
+            }
+        }
 
-          return Container(
-            height: MediaQuery.of(context).size.height / 3,
-            margin: EdgeInsets.symmetric(horizontal: 10),
-            child: Card(
-              elevation: 10,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(16),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: child,
-              ),
-            ),
-          );
-        });
+        return Container(
+          width: 250,
+          height: 350,
+          color: Colors.blueGrey,
+          child: child,
+        );
+      },
+    );
   }
 }
