9fc5e123b99db42f2805b294038992bfdd0ddb38
Nader2004 <71600566+Nader2004@users.noreply.github.com>
2021-04-13 19:12:48 +0200

    Update post-native-ad.dart

--- lib/widgets/post-native-ad.dart
+++ lib/widgets/post-native-ad.dart
@@ -1,7 +1,6 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:native_ads/native_ad_param.dart';
+import 'package:native_ads/native_ad_view.dart';
 
 class PostNativeAd extends StatefulWidget {
   PostNativeAd({Key key}) : super(key: key);
@@ -15,67 +14,24 @@ class _PostNativeAdState extends State<PostNativeAd> {
       'ca-app-pub-3940256099942544/2247696110'; // 'ca-app-pub-9448985372006294/2961269368';
   static const String IOSAdUnitId =
       'ca-app-pub-3940256099942544/3986624511'; //'ca-app-pub-9448985372006294/6816753892';
-  NativeAd _nativeAd;
-  final Completer<NativeAd> nativeAdCompleter = Completer<NativeAd>();
-
-  @override
-  void initState() {
-    super.initState();
-    _nativeAd = NativeAd(
-      adUnitId: NativeAd.testAdUnitId,
-      request: AdRequest(),
-      factoryId: 'native-ad',
-      listener: AdListener(
-        onAdLoaded: (Ad ad) {
-          print('$NativeAd loaded.');
-          nativeAdCompleter.complete(ad as NativeAd);
-        },
-        onAdFailedToLoad: (Ad ad, LoadAdError error) {
-          ad.dispose();
-          print('$NativeAd failedToLoad: $error');
-          nativeAdCompleter.completeError(error);
-        },
-        onAdOpened: (Ad ad) => print('$NativeAd onAdOpened.'),
-        onAdClosed: (Ad ad) => print('$NativeAd onAdClosed.'),
-        onApplicationExit: (Ad ad) => print('$NativeAd onApplicationExit.'),
-      ),
-    );
-    Future<void>.delayed(Duration(seconds: 1), () => _nativeAd.load());
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    _nativeAd.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<NativeAd>(
-      future: nativeAdCompleter.future,
-      builder: (BuildContext context, AsyncSnapshot<NativeAd> snapshot) {
-        Widget child;
-
-        switch (snapshot.connectionState) {
-          case ConnectionState.none:
-          case ConnectionState.waiting:
-          case ConnectionState.active:
-            child = Container();
-            break;
-          case ConnectionState.done:
-            if (snapshot.hasData) {
-              child = AdWidget(ad: _nativeAd);
-            } else {
-              child = Text('Error loading $NativeAd');
-            }
-        }
-
-        return Container(
-          width: 250,
-          height: 250,
-          child: child,
-        );
-      },
+    return Container(
+      width: 250,
+      height: 250,
+      child: NativeAdView(
+        androidParam: AndroidParam()
+          ..placementId = 'ca-app-pub-3940256099942544/2247696110'
+          ..packageName = 'com.app.looper'
+          ..layoutName = 'native_ad'
+          ..attributionText = 'SPONSORED',
+        iosParam: IOSParam()
+          ..placementId = 'ca-app-pub-3940256099942544/3986624511'
+          ..bundleId = 'com.app.looper'
+          ..layoutName = 'UnifiedNativeAdView'
+          ..attributionText = 'SPONSORED',
+      ),
     );
   }
 }
