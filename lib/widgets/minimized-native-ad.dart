9d153aaf691b22129f435527ea211e80f2451bdb
Nader2004 <71600566+Nader2004@users.noreply.github.com>
2021-04-13 19:13:16 +0200

    Update minimized-native-ad.dart

--- lib/widgets/minimized-native-ad.dart
+++ lib/widgets/minimized-native-ad.dart
@@ -1,7 +1,6 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:native_ads/native_ad_param.dart';
+import 'package:native_ads/native_ad_view.dart';
 
 class MinimizedNativeAd extends StatefulWidget {
   MinimizedNativeAd({Key key}) : super(key: key);
@@ -14,48 +13,27 @@ class _MinimizedNativeAdState extends State<MinimizedNativeAd> {
   static const String AndroidAdUnitId =
       'ca-app-pub-9448985372006294/2961269368';
   static const String IOSAdUnitId = 'ca-app-pub-9448985372006294/6816753892';
-  NativeAd nativeAd;
-  bool _isLoaded = false;
-
-  @override
-  void initState() {
-    nativeAd = NativeAd(
-      adUnitId: Platform.isIOS ? IOSAdUnitId : AndroidAdUnitId,
-      factoryId: 'native-ad',
-      listener: AdListener(
-        onAdLoaded: (ad) {
-          setState(() {
-            _isLoaded = true;
-          });
-        },
-        onAdFailedToLoad: (ad, error) {
-          nativeAd.dispose();
-        },
-        onApplicationExit: (ad) {
-          nativeAd.dispose();
-        },
-      ),
-      request: AdRequest(),
-    );
-    nativeAd.load();
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    nativeAd.dispose();
-    super.dispose();
-  }
+ 
 
   @override
   Widget build(BuildContext context) {
-    final AdWidget adWidget = AdWidget(ad: nativeAd);
-    return _isLoaded == false ? SizedBox.shrink() : Container(
+     return  Container(
       height: MediaQuery.of(context).size.height / 5,
       margin: EdgeInsets.symmetric(horizontal: 10),
       child: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: adWidget,
+        child: NativeAdView(
+            androidParam: AndroidParam()
+              ..placementId = 'ca-app-pub-3940256099942544/2247696110'
+              ..packageName = 'com.app.looper'
+              ..layoutName = 'native_ad'
+              ..attributionText = 'SPONSORED',
+            iosParam: IOSParam()
+              ..placementId = 'ca-app-pub-3940256099942544/3986624511'
+              ..bundleId = 'com.app.looper'
+              ..layoutName = 'UnifiedNativeAdView'
+              ..attributionText = 'SPONSORED',
+          ),
       ),
     );
   }
