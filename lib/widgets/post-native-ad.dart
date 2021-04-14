97dbafd3ef80e369c6195fba89a350585c081cc6
Nader2004 <71600566+Nader2004@users.noreply.github.com>
2021-04-14 11:40:02 +0200

    Update post-native-ad.dart

--- lib/widgets/post-native-ad.dart
+++ lib/widgets/post-native-ad.dart
@@ -1,6 +1,8 @@
+import 'dart:async';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
-import 'package:native_ads/native_ad_param.dart';
-import 'package:native_ads/native_ad_view.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 class PostNativeAd extends StatefulWidget {
   PostNativeAd({Key key}) : super(key: key);
@@ -14,24 +16,73 @@ class _PostNativeAdState extends State<PostNativeAd> {
       'ca-app-pub-3940256099942544/2247696110'; // 'ca-app-pub-9448985372006294/2961269368';
   static const String IOSAdUnitId =
       'ca-app-pub-3940256099942544/3986624511'; //'ca-app-pub-9448985372006294/6816753892';
+  NativeAd nativeAd;
+  final Completer<NativeAd> nativeAdCompleter = Completer<NativeAd>();
+
+  @override
+  void initState() {  
+    nativeAd = NativeAd(
+      adUnitId: Platform.isIOS ? IOSAdUnitId : AndroidAdUnitId,
+      factoryId: 'native-ad',
+      listener: AdListener(
+        onAdLoaded: (ad) {
+          nativeAdCompleter.complete(ad as NativeAd);
+        },
+        onAdFailedToLoad: (ad, error) {
+          nativeAd.dispose();
+          nativeAdCompleter.completeError(error);
+        },
+        onApplicationExit: (ad) {
+          nativeAd.dispose();
+        },
+      ),
+      request: AdRequest(),
+    );
+    Future<void>.delayed(Duration(seconds: 1), () => nativeAd.load());
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    nativeAd.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 250,
-      height: 250,
-      child: NativeAdView(
-        androidParam: AndroidParam()
-          ..placementId = 'ca-app-pub-3940256099942544/2247696110'
-          ..packageName = 'com.app.looper'
-          ..layoutName = 'native_ad'
-          ..attributionText = 'SPONSORED',
-        iosParam: IOSParam()
-          ..placementId = 'ca-app-pub-3940256099942544/3986624511'
-          ..bundleId = 'com.app.looper'
-          ..layoutName = 'UnifiedNativeAdView'
-          ..attributionText = 'SPONSORED',
-      ),
-    );
+    return FutureBuilder<NativeAd>(
+        future: nativeAdCompleter.future,
+        builder: (context, snapshot) {
+          Widget child;
+
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+            case ConnectionState.waiting:
+            case ConnectionState.active:
+              child = Container();
+              break;
+            case ConnectionState.done:
+              if (snapshot.hasData) {
+                child = AdWidget(ad: nativeAd);
+              } else {
+                child = Text('Error loading $NativeAd');
+              }
+          }
+
+          return Container(
+            height: MediaQuery.of(context).size.height / 3,
+            margin: EdgeInsets.symmetric(horizontal: 10),
+            child: Card(
+              elevation: 10,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: child,
+              ),
+            ),
+          );
+        });
   }
 }
