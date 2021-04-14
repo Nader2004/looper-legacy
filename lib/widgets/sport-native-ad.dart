bc1e4b4fc38f52ce1a1ea556176e9098d785d829
Nader2004 <71600566+Nader2004@users.noreply.github.com>
2021-04-14 11:40:28 +0200

    Update sport-native-ad.dart

--- lib/widgets/sport-native-ad.dart
+++ lib/widgets/sport-native-ad.dart
@@ -1,6 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
-import 'package:native_ads/native_ad_param.dart';
-import 'package:native_ads/native_ad_view.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 class SportNativeAd extends StatefulWidget {
   SportNativeAd({Key key}) : super(key: key);
@@ -9,36 +10,54 @@ class SportNativeAd extends StatefulWidget {
   _SportNativeAdState createState() => _SportNativeAdState();
 }
 
-class _SportNativeAdState extends State<SportNativeAd>
-    with AutomaticKeepAliveClientMixin {
+class _SportNativeAdState extends State<SportNativeAd> {
   static const String AndroidAdUnitId =
       'ca-app-pub-9448985372006294/2961269368';
   static const String IOSAdUnitId = 'ca-app-pub-9448985372006294/6816753892';
+  NativeAd nativeAd;
+  bool _isLoaded = false;
 
-  bool get wantKeepAlive => true;
+  @override
+  void initState() {
+    nativeAd = NativeAd(
+      adUnitId: Platform.isIOS ? IOSAdUnitId : AndroidAdUnitId,
+      factoryId: 'native-ad',
+      listener: AdListener(
+        onAdLoaded: (ad) {
+          setState(() {
+            _isLoaded = true;
+          });
+        },
+        onAdFailedToLoad: (ad, error) {
+          nativeAd.dispose();
+        },
+        onApplicationExit: (ad) {
+          nativeAd.dispose();
+        },
+      ),
+      request: AdRequest(),
+    );
+    nativeAd.load();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    nativeAd.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    super.build(context);
-    return Container(
+    final AdWidget adWidget = AdWidget(ad: nativeAd);
+    return _isLoaded == false ? SizedBox.shrink() : Container(
       height: MediaQuery.of(context).size.height / 1.8,
       margin: EdgeInsets.symmetric(horizontal: 10),
       child: ClipRRect(
         borderRadius: BorderRadius.circular(16),
         child: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: NativeAdView(
-            androidParam: AndroidParam()
-              ..placementId = 'ca-app-pub-3940256099942544/2247696110'
-              ..packageName = 'com.app.looper'
-              ..layoutName = 'native_ad'
-              ..attributionText = 'SPONSORED',
-            iosParam: IOSParam()
-              ..placementId = 'ca-app-pub-3940256099942544/3986624511'
-              ..bundleId = 'com.app.looper'
-              ..layoutName = 'UnifiedNativeAdView'
-              ..attributionText = 'SPONSORED',
-          ),
+          child: adWidget,
         ),
       ),
     );
