03175b7b0fe2706de6336f92d1bdcadb5306e258
Nader <71600566+Nader2004@users.noreply.github.com>
2021-04-16 10:07:55 +0200

    Update minimized-native-ad.dart

--- lib/widgets/minimized-native-ad.dart
+++ lib/widgets/minimized-native-ad.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:admob_flutter/admob_flutter.dart';
 
 class MinimizedNativeAd extends StatefulWidget {
   MinimizedNativeAd({Key key}) : super(key: key);
@@ -14,48 +14,17 @@ class _MinimizedNativeAdState extends State<MinimizedNativeAd> {
   static const String AndroidAdUnitId =
       'ca-app-pub-9448985372006294/2961269368';
   static const String IOSAdUnitId = 'ca-app-pub-9448985372006294/6816753892';
-  NativeAd nativeAd;
-  bool _isLoaded = false;
-
-  @override
-  void initState() {
-    nativeAd = NativeAd(
-      adUnitId: Platform.isIOS ? IOSAdUnitId : AndroidAdUnitId,
-      factoryId: 'native-ad',
-      listener: AdListener(
-        onAdLoaded: (ad) {
-          setState(() {
-            _isLoaded = true;
-          });
-        },
-        onAdFailedToLoad: (ad, error) {
-          nativeAd.dispose();
-        },
-        onApplicationExit: (ad) {
-          nativeAd.dispose();
-        },
-      ),
-      request: AdRequest(),
-    );
-    nativeAd.load();
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    nativeAd.dispose();
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
-    final AdWidget adWidget = AdWidget(ad: nativeAd);
-    return _isLoaded == false ? SizedBox.shrink() : Container(
-      height: MediaQuery.of(context).size.height / 5,
+    return Container(
       margin: EdgeInsets.symmetric(horizontal: 10),
       child: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: adWidget,
+        child: AdmobBanner(
+          adUnitId: Platform.isIOS ? IOSAdUnitId : AndroidAdUnitId,
+          adSize: AdmobBannerSize.BANNER,
+        ),
       ),
     );
   }
