ea2c3de136300c3028e814f49af61a819fd6aa36
Nader <71600566+Nader2004@users.noreply.github.com>
2021-04-16 07:18:44 +0200

    Update post-native-ad.dart

--- lib/widgets/post-native-ad.dart
+++ lib/widgets/post-native-ad.dart
@@ -1,8 +1,5 @@
-import 'dart:async';
-import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:admob_flutter/admob_flutter.dart';
 
 class PostNativeAd extends StatefulWidget {
   PostNativeAd({Key key}) : super(key: key);
@@ -16,73 +13,33 @@ class _PostNativeAdState extends State<PostNativeAd> {
       'ca-app-pub-3940256099942544/2247696110'; // 'ca-app-pub-9448985372006294/2961269368';
   static const String IOSAdUnitId =
       'ca-app-pub-3940256099942544/3986624511'; //'ca-app-pub-9448985372006294/6816753892';
-  NativeAd nativeAd;
-  final Completer<NativeAd> nativeAdCompleter = Completer<NativeAd>();
 
   @override
-  void initState() {  
-    nativeAd = NativeAd(
-      adUnitId: Platform.isIOS ? IOSAdUnitId : AndroidAdUnitId,
-      factoryId: 'native-ad',
-      listener: AdListener(
-        onAdLoaded: (ad) {
-          nativeAdCompleter.complete(ad as NativeAd);
-        },
-        onAdFailedToLoad: (ad, error) {
-          nativeAd.dispose();
-          nativeAdCompleter.completeError(error);
-        },
-        onApplicationExit: (ad) {
-          nativeAd.dispose();
-        },
-      ),
-      request: AdRequest(),
-    );
-    Future<void>.delayed(Duration(seconds: 1), () => nativeAd.load());
+  void initState() {
     super.initState();
   }
 
   @override
-  void dispose() {
-    nativeAd.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return FutureBuilder<NativeAd>(
-        future: nativeAdCompleter.future,
-        builder: (context, snapshot) {
-          Widget child;
-
-          switch (snapshot.connectionState) {
-            case ConnectionState.none:
-            case ConnectionState.waiting:
-            case ConnectionState.active:
-              child = Container();
-              break;
-            case ConnectionState.done:
-              if (snapshot.hasData) {
-                child = AdWidget(ad: nativeAd);
-              } else {
-                child = Text('Error loading $NativeAd');
-              }
-          }
-
-          return Container(
-            height: MediaQuery.of(context).size.height / 3,
-            margin: EdgeInsets.symmetric(horizontal: 10),
-            child: Card(
-              elevation: 10,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(16),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: child,
-              ),
+    return Container(
+      height: MediaQuery.of(context).size.height / 3,
+      margin: EdgeInsets.symmetric(horizontal: 10),
+      child: Card(
+        elevation: 10,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            margin: EdgeInsets.only(bottom: 20.0),
+            child: AdmobBanner(
+              adUnitId: 'ca-app-pub-3940256099942544/2934735716',
+              adSize: AdmobBannerSize.BANNER,
             ),
-          );
-        });
+          ),
+        ),
+      ),
+    );
   }
 }
