7879e4da888bb15f202943e8ba907e5221f0ae1d
Nader <71600566+Nader2004@users.noreply.github.com>
2021-06-25 16:26:32 +0200

    Update minimized-native-ad.dart

--- lib/widgets/minimized-native-ad.dart
+++ lib/widgets/minimized-native-ad.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:admob_flutter/admob_flutter.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 class MinimizedNativeAd extends StatefulWidget {
   MinimizedNativeAd({Key key}) : super(key: key);
@@ -14,17 +14,38 @@ class _MinimizedNativeAdState extends State<MinimizedNativeAd> {
   static const String AndroidAdUnitId =
       'ca-app-pub-9448985372006294/2961269368';
   static const String IOSAdUnitId = 'ca-app-pub-9448985372006294/6816753892';
+  NativeAd nativeAd;
+
+  @override
+  void initState() {
+    super.initState();
+    nativeAd = NativeAd(
+      adUnitId: Platform.isIOS ? IOSAdUnitId : AndroidAdUnitId,
+      factoryId: 'native-ad',
+      listener: AdListener(
+        onAdFailedToLoad: (Ad ad, LoadAdError error) {
+          nativeAd.dispose();
+        },
+      ),
+      request: AdRequest(),
+    );
+    nativeAd.load();
+  }
+
+  @override
+  void dispose() {
+    nativeAd.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
+    final AdWidget adWidget = AdWidget(ad: nativeAd);
     return Container(
       margin: EdgeInsets.symmetric(horizontal: 10),
       child: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: AdmobBanner(
-          adUnitId: Platform.isIOS ? IOSAdUnitId : AndroidAdUnitId,
-          adSize: AdmobBannerSize.BANNER,
-        ),
+        child: adWidget,
       ),
     );
   }
